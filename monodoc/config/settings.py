aa1ebbd1a98243fd5ffed48d62b7a8e8f0c95a5b
hfxz <xuan__zai@outlook.com>
2025-12-02 14:02:10 +0800

    feat: add config module
    - use pydantic-settings to setup configs

---
+++ monodoc/config/settings.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "monodoc"
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
